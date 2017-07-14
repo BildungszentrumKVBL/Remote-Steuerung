3598061a0620c8b540383200321a5040b604d321
CoalaJoe <someone@somewhere.com>
2017-07-14 09:20:16 +0200

    Added cache clear

--- src/AppBundle/Service/SettingsHandler.php
+++ src/AppBundle/Service/SettingsHandler.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Service;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Yaml\Yaml;
 
@@ -28,13 +29,19 @@ class SettingsHandler
     private $kernelRoot;
 
     /**
+     * @var ContainerInterface $container
+     */
+    private $container;
+
+    /**
      * SettingsHandler constructor.
      *
      * @param KernelInterface $kernel
      */
     public function __construct(KernelInterface $kernel)
     {
-        $this->kernelRoot = $kernel->getContainer()->getParameter('kernel.root_dir');
+        $this->container  = $kernel->getContainer();
+        $this->kernelRoot = $this->container->getParameter('kernel.root_dir');
         $this->settings   = Yaml::parse(file_get_contents($this->kernelRoot.self::SETTINGS_FILE))['parameters'];
     }
 
@@ -50,7 +57,7 @@ class SettingsHandler
     {
         foreach ($settings as $name => $value) {
             if (array_key_exists($name, $this->settings)) {
-                if (is_numeric($value)) {
+                if (is_numeric($value) && !(substr($value, 0, 1) === '+' || substr($value, 0, 1) === '0')) {
                     $value = (int) $value;
                 } elseif ($value === "true") {
                     $value = true;
@@ -61,5 +68,16 @@ class SettingsHandler
             }
         }
         file_put_contents($this->kernelRoot.self::SETTINGS_FILE, Yaml::dump(['parameters' => $this->settings]));
+        $this->clearCache();
+    }
+
+    private function clearCache()
+    {
+        if (!$this->container->getParameter('kernel.debug')) {
+            $cacheFile = $this->container->getParameter('kernel.cache_dir').'/appProdProjectContainer.php';
+            if (file_exists($cacheFile)) {
+                unlink($cacheFile);
+            }
+        }
     }
 }
