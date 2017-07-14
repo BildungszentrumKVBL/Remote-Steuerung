40187836da281acdae178439fe61b1733591beca
CoalaJoe <someone@somewhere.com>
2017-07-14 10:20:27 +0200

    Removed cache clearer

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -149,11 +149,6 @@ class AdminController extends Controller
             }
         }
 
-        $filesystem = $this->get('filesystem');
-        $realCacheDir = $this->getParameter('kernel.cache_dir');
-        $this->get('cache_clearer')->clear($realCacheDir);
-        $filesystem->remove($realCacheDir);
-
         return $this->render('AppBundle:admin:settings.html.twig', ['settings' => $settings]);
     }
 
